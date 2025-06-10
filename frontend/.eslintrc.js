f650f2a07ab2ed19da7dc03b33bf2f347e7a91e1
Sunslihgt <samsam.deschamps@gmail.com>
2025-06-10 17:09:29 +0200

    Initial commit

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'quotes': ['warn', 'single'],
+    'semi': ['off'],
+    'max-len': ['warn', { code: 100, tabWidth: 2 }],
+    'no-console': 'warn',
+    '@typescript-eslint/semi': ['warn', 'always'],
+    '@typescript-eslint/quotes': ['warn', 'single'],
+    'indent': ['warn', 4],
+  },
+};
---
+++ frontend/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+// import App from './App';
+
+const container = document.getElementById('root');
+if (container === null) {
+    throw new Error('Something went wrong!');
+}
+
+const root = createRoot(container)
+
+root.render(
+    <React.StrictMode>
+        <h1>Hello world</h1>
+        {/* <App /> */}
+    </React.StrictMode>
+);
