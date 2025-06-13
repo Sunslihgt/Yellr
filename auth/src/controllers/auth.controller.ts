695df105fbc1c0fe56f243b053acf6e83b8209eb
Sunslihgt <samsam.deschamps@gmail.com>
2025-06-13 16:11:00 +0200

    Finished fixing the project configuration Fixed docker-compose, Dockerfiles and Nginx. Added launch bash files and. Now using nodemon.

--- auth/src/controllers/auth.controller.ts
+++ auth/src/controllers/auth.controller.ts
@@ -19,19 +19,16 @@ export const register = async (
     req: Request<{}, {}, AuthBody>,
     res: Response
 ) => {
-    // if (!req.body || !req.body.email || !req.body.password) {
-    //     return res.status(400).json({ msg: 'Email and password are required.' });
-    // }
-    // const newUserAuth = new Auth(
-    //     req.body.email,
-    //     hashSync(req.body.password, 10)
-    // );
-    // User_DB.push(newUserAuth);
-    // return res.status(201).json({
-    //     msg: 'New User created !'
-    // });
+    if (!req.body || !req.body.email || !req.body.password) {
+        return res.status(400).json({ msg: 'Email and password are required.' });
+    }
+    const newUserAuth = new Auth(
+        req.body.email,
+        hashSync(req.body.password, 10)
+    );
+    User_DB.push(newUserAuth);
     return res.status(201).json({
-        msg: 'Register route is working'
+        msg: 'New User created !'
     });
 };
 
@@ -57,9 +54,9 @@ export const login = async (
 
         return res.status(200).json({ message: `You are now connected, token: ${accessToken}` });
     } else if (!process.env.ACCESS_JWT_KEY) {
-        return res.status(401).json({ message: "Invalid JWT key" });
+        return res.status(401).json({ message: 'Invalid JWT key' });
     } else {
-        return res.status(401).json({ message: "Invalid credentials" });
+        return res.status(401).json({ message: 'Invalid credentials' });
     }
 };
 
@@ -68,16 +65,16 @@ export const authenticate = async (
     res: Response
 ) => {
     console.log('In authenticate');
-    let tokenString = req.headers["authorization"];
+    let tokenString = req.headers['authorization'];
 
     if (!tokenString?.startsWith('Bearer ') || tokenString.split('Bearer ').length < 2) {
-        return res.status(401).json({ message: "Token absent or malformed" });
+        return res.status(401).json({ message: 'Token absent or malformed' });
     }
 
     const token = tokenString.split('Bearer ')[1];
 
     if (!process.env.ACCESS_JWT_KEY) {
-        return res.status(401).json({ message: "Invalid JWT key" });
+        return res.status(401).json({ message: 'Invalid JWT key' });
     }
 
     verify(
@@ -85,17 +82,17 @@ export const authenticate = async (
         process.env.ACCESS_JWT_KEY,
         (err: any, decoded: any) => {
             if (err) {
-                return res.status(403).json({ message: "Invalid or expired token" });
+                return res.status(403).json({ message: 'Invalid or expired token' });
             }
 
             // Vérification si l’utilisateur décodé existe
             const user = User_DB.find(u => u.email === decoded.email);
             if (!user) {
-                return res.status(404).json({ message: "User not found" });
+                return res.status(404).json({ message: 'User not found' });
             }
 
             // Renvoyer une réponse adaptée
-            return res.status(200).json({ message: "Token is valid", user: decoded.email });
+            return res.status(200).json({ message: 'Token is valid', user: decoded.email });
         }
     );
 };
---
+++ run-dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export NODE_ENV=development
+
+echo "========================================="
+echo "Stopping and removing all containers"
+echo "========================================="
+
+docker-compose down --remove-orphans
+
+echo "========================================="
+echo "Starting all containers in ${NODE_ENV} mode"
+echo "========================================="
+
+docker-compose up --build
