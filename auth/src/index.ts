3e0f055ed6239eb13230953b0899192d5016dbbf
Sunslihgt <samsam.deschamps@gmail.com>
2025-06-18 11:29:44 +0200

    Revert "WIP Prisma and MySQL"
    This reverts commit 243840f14e76e0a299a16412c68a4f4b9d957fe9.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,9 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes';
-import prisma from './utils/prisma';
 
-dotenv.config({ path: './.env' });
+dotenv.config({ path: './src/.env' });
 
 const app = express();
 app.use(cors());
@@ -16,10 +15,7 @@ const PORT = 5000;
 app.use('/', authRoutes);
 
 app.get('/test', (req, res) => {
-    // res.send('auth-service: API is running');
-    prisma.user.findMany().then((users: any) => {
-        res.json(users);
-    });
+    res.send('auth-service: API is running');
 });
 
 app.listen(PORT, () => {
