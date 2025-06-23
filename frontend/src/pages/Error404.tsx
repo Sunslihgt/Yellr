c3cfe03c2d9778c606230aa3df9b072038e6dcc0
Sunslihgt <samsam.deschamps@gmail.com>
2025-06-23 11:28:37 +0200

    Frontend Pierre > fixes + Pride

--- frontend/src/pages/Error404.tsx
+++ frontend/src/pages/Error404.tsx
@@ -1,22 +1,20 @@
 function Error() {
     return (
-        <div className="bg-gray-100 dark:bg-gray-800 min-h-[calc(100vh-8rem)] flex items-center justify-center px-6 py-24 sm:py-32 lg:px-8">
+        <div className="flex-1 flex flex-col justify-center bg-gray-100 dark:bg-gray-800">
             <div className="text-center">
-                <p className="text-base font-semibold text-gray-900 dark:text-white">404</p>
-                <h1 className="mt-4 text-5xl font-semibold tracking-tight text-gray-900 dark:text-white sm:text-7xl">
+                <p className="text-4xl font-bold text-gray-900 dark:text-white">404</p>
+                <h1 className="mt-4 text-3xl font-semibold text-gray-900 dark:text-white">
                     Page not found
                 </h1>
-                <p className="mt-6 text-lg font-medium text-gray-600 dark:text-gray-300 sm:text-xl/8">
-                    Sorry, we couldn’t find the page you’re looking for.
+                <p className="mt-4 text-lg text-gray-600 dark:text-gray-300">
+                    Sorry, we couldn&apos;t find the page you&apos;re looking for.
                 </p>
-                <div className="mt-10 flex items-center justify-center gap-x-6">
-                    <a
-                        href="/"
-                        className="rounded-md bg-blue-600 hover:bg-blue-500 px-4 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                    >
-                        Go back home
-                    </a>
-                </div>
+                <a
+                    href="/"
+                    className="mt-8 inline-block rounded-md bg-blue-600 hover:bg-blue-500 px-4 py-2 text-sm font-semibold text-white shadow"
+                >
+                    Go back home
+                </a>
             </div>
         </div>
     );
